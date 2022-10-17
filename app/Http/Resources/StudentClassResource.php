9b6dce9c1d0b728ad6ab73286b1827a0d5f90c4a
Rafiqul Islam Tusher <md.rafiqul26997@gmail.com>
2022-10-17 11:44:07 +0600

    Class Id Showing

--- app/Http/Resources/StudentClassResource.php
+++ app/Http/Resources/StudentClassResource.php
@@ -15,6 +15,7 @@ class StudentClassResource extends JsonResource
     public function toArray($request)
     {
         return [
+            'id' => $this->id,
             'name' => $this->name,
             'status'=>$this->status
         ];
